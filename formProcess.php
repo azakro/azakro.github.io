d61d9b3ba301cb0d13aa59430e5c9074b8b66c40
Alana <alanazak8@gmail.com>
2016-12-11 13:09:09 -0500

    update formProcess.php

--- formProcess.php
+++ formProcess.php
@@ -1,29 +1,14 @@
 
 <?php
-	$emailFrom = Trim(stripslashes($_POST["email"]));
-	$emailTo = "alanazak8@gmail.com";
 	
-	function died($error) {
- 
-        // your error code can go here
- 
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
- 
-        echo "These errors appear below.<br /><br />";
- 
-        echo $error."<br /><br />";
- 
-        echo "Please go back and fix these errors.<br /><br />";
- 
-        die();
- 
-    }
+	$to = "alanazak8@gmail.com";
+	$subject = "My subject";
+	$txt = "Hello world!";
+	$headers = "From: webmaster@example.com" . "\r\n" .
+	"CC: somebodyelse@example.com";
 
-	$name = Trim(stripslashes($_POST["name"]));
-	$message = Trim(stripslashes($_POST["message"]));
-	$emailBody = "Name: $name \n Email: $emailTo \n\n Message: \n $message \n";
+	mail($to,$subject,$txt,$headers);
 
-	mail($emailTo, "From personal website", $emailBody, "From:<$emailFrom>");
 	
 	header('Location: success.html');
 
