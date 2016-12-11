9ba98914c8499f1d86f8c53841b21fff362ccaa1
Alana <alanazak8@gmail.com>
2016-12-11 12:33:43 -0500

    update formProcess.php

--- formProcess.php
+++ formProcess.php
@@ -1,12 +1,11 @@
+
 <?php
-	$emailFrom = "alanazak8@gmail.com";
-	$emailTo = Trim(stripslashes($_POST["email"]));
+	$emailFrom = Trim(stripslashes($_POST["email"]));
+	$emailTo = "alanazak8@gmail.com";
 	$name = Trim(stripslashes($_POST["name"]));
-	$subject = "From Website";
 	$message = Trim(stripslashes($_POST["message"]));
-	$emailBody = "Name: $name \n Email: $emailTo \n Phone: $phoneNum \n Browsers Used: \n $chromeCB \n $firefoxCB \n $safariCB \n $ieCB \n $edgeCB \n\n Favorite Browser: $radioCB \n\n Message: \n $message \n";
+	$emailBody = "Name: $name \n Email: $emailTo \n\n Message: \n $message \n";
 
 	mail($emailTo, "Email to you", $emailBody, "From:<$emailFrom>");
 	header("Location: success.html");
-	exit;
 ?>
